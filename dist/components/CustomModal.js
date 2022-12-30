bc1d8f167b00567e5262f5fe3ee3e0ed12891968
SFlorian95 <subramanyam.florian@outlook.fr>
2022-12-30 20:10:52 +0100

    readme file updated

--- dist/components/CustomModal.js
+++ dist/components/CustomModal.js
@@ -4,24 +4,26 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
+var _react = _interopRequireDefault(require("react"));
 require("./CustomModal.scss");
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 const CustomModal = _ref => {
   let {
     isDisplayed = false,
     onCloseModal,
     content
   } = _ref;
-  return /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/_react.default.createElement("div", {
     className: "custom-modal",
     style: {
       display: isDisplayed ? 'block' : 'none'
     }
-  }, /*#__PURE__*/React.createElement("div", {
+  }, /*#__PURE__*/_react.default.createElement("div", {
     className: "custom-modal-main"
-  }, /*#__PURE__*/React.createElement("div", {
+  }, /*#__PURE__*/_react.default.createElement("div", {
     onClick: onCloseModal,
     className: "custom-modal-btn-close"
-  }, "X"), /*#__PURE__*/React.createElement("div", {
+  }, "X"), /*#__PURE__*/_react.default.createElement("div", {
     className: "custom-modal-content"
   }, content)));
 };
